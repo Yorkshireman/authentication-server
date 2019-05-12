93f74d5c5beb4156ec2c2655cc64a178d7e6a637
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-05-12 22:36:35 +0100

    Revert config/application cors changes until it is needed, if ever

--- config/application.rb
+++ config/application.rb
@@ -14,19 +14,15 @@ Bundler.require(*Rails.groups)
 module AuthenticationServer
   class Application < Rails::Application
     config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
-
-    #autoloads lib folder during production
-    config.eager_load_paths << Rails.root.join('lib')
-
-    #autoloads lib folder during development
-    config.autoload_paths << Rails.root.join('lib')
   end
 end
