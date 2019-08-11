a6e896d850bfbd9df84aef9da6ab788cdb3b97df
Andrew Stelmach <Yorkshireman@users.noreply.github.com>
2019-08-12 00:35:50 +0100

    Fix CORS for deploys (#2)
    * remove CORS_URLS - hardcode regex

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['FRONTEND_CLIENT_URL'] || ''
+    origins 'http://localhost:3000',
+            /https:\/\/mywordlist-[a-z0-9-]+.herokuapp.com/
 
     resource '*',
       headers: :any,
