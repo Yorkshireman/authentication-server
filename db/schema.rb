1dac4add72bcf774315913411fb3a41c2e6717d0
Andrew Stelmach <Yorkshireman@users.noreply.github.com>
2019-08-12 00:41:14 +0100

    Authentication requests (#1)
    * register, login, and authenticate requests

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_22_163003) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "All Users deleted" if User.destroy_all
+puts "Joe Bloggs created" if User.create({ name: 'Joe Bloggs', email: 'joebloggs@hotmail.com', password: 'joebloggs' })
+puts "Farmer Bob created" if User.create({ name: 'Farmer Bob', email: 'farmerbob@hotmail.com', password: 'farmerbob' })
