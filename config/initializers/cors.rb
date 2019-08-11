35f139f3739556c0ebbc6641431d8ebbd078c3ff
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-08-11 23:54:56 +0100

    cors urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['FRONTEND_CLIENT_URL'] || ''
+    origins ENV['CORS_URLS'] || ''
 
     resource '*',
       headers: :any,
