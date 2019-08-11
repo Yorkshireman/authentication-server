09553c80fcc8b32e20f6268cd3e41b6757f525bd
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-08-11 13:23:46 +0100

    Add fallback to FRONTEND_CLIENT_URL in code instead

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['FRONTEND_CLIENT_URL']
+    origins ENV['FRONTEND_CLIENT_URL'] || ''
 
     resource '*',
       headers: :any,
