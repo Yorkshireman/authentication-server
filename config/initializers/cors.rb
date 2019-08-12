8e34d66a90aca76832c54b4db64aeaad9cb5a1f6
Andrew Stelmach <Yorkshireman@users.noreply.github.com>
2019-08-12 22:19:51 +0100

    Fix regex (#4)
    Heroku app names generated for the frontend client do not always start with my-wordlist

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000',
-            /https:\/\/mywordlist-[a-z0-9-]+.herokuapp.com/
+            /https:\/\/[a-z0-9-]+.herokuapp.com/
 
     resource '*',
       headers: :any,
