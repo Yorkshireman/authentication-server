9084757eda85e2a58a5fcf69c1ed17346e1bd480
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-09-30 00:02:29 +0100

    refactor tests and write one for user creation

--- spec/requests/signup_spec.rb
+++ spec/requests/signup_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'POST /signup', type: :request do
     it 'something happens'
   end
 
-  describe 'response to a valid request' do
+  describe 'when request is valid' do
     before :each do
       headers = {
         'CONTENT_TYPE' => 'application/vnd.api+json'
@@ -23,29 +23,35 @@ RSpec.describe 'POST /signup', type: :request do
       post '/signup', headers: headers, params: params
     end
 
-    it 'has correct Content-Type header value' do
-      expect(response.content_type).to eq('application/vnd.api+json')
+    it 'creates a user' do
+      expect(User.all.length).to eq(1)
     end
 
-    it 'has 201 status code' do
-      expect(response).to have_http_status(201)
-    end
-
-    describe 'token' do
-      it 'is a String' do
-        expect(JSON.parse(response.body)['data']['token']).to be_a(String)
+    describe 'response' do
+      it 'has correct Content-Type header value' do
+        expect(response.content_type).to eq('application/vnd.api+json')
       end
 
-      it 'contains correct information' do
-        expected_decoded_token = [{ 'user_id' => '1' }, { 'alg' => 'HS256' }]
-        actual_decoded_token = JWT.decode(
-          JSON.parse(response.body)['data']['token'],
-          ENV['JWT_SECRET_KEY'],
-          true,
-          { algorithm: 'HS256' }
-        )
+      it 'has 201 status code' do
+        expect(response).to have_http_status(201)
+      end
 
-        expect(actual_decoded_token).to eq(expected_decoded_token)
+      describe 'token' do
+        it 'is a String' do
+          expect(JSON.parse(response.body)['data']['token']).to be_a(String)
+        end
+
+        it 'contains correct information' do
+          expected_decoded_token = [{ 'user_id' => '1' }, { 'alg' => 'HS256' }]
+          actual_decoded_token = JWT.decode(
+            JSON.parse(response.body)['data']['token'],
+            ENV['JWT_SECRET_KEY'],
+            true,
+            { algorithm: 'HS256' }
+          )
+
+          expect(actual_decoded_token).to eq(expected_decoded_token)
+        end
       end
     end
   end
