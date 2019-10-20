ff429b80569e83662de2079e723872219f66a251
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-10-20 18:10:05 +0100

    Remove env var comment

--- app/helpers/token_helper.rb
+++ app/helpers/token_helper.rb
@@ -2,7 +2,7 @@ require 'jwt'
 
 module TokenHelper
   def generate_token(payload)
-    secret_key = ENV['JWT_SECRET_KEY'] # can this be moved into config so it's less expensive?
+    secret_key = ENV['JWT_SECRET_KEY']
     JWT.encode(payload, secret_key, 'HS256')
   end
 end
