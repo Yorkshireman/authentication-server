58d11ed58dd6568ef36b31bc45e11e5caa61bfeb
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2019-11-25 23:33:24 +0000

    add unique email index to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_230740) do
+ActiveRecord::Schema.define(version: 2019_11_25_233121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_09_29_230740) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
