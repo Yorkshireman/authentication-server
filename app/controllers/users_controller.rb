14ec5fa801afb45c2faa7d2fd226e4afe8660562
Andrew Stelmach <Yorkshireman@users.noreply.github.com>
2019-11-27 19:29:09 +0000

    15 signin (#16)
    * Create /signin route
    
    * make email unique for User model
    
    * add unique email index to users
    
    * write and pass test for uniqueness test on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,44 @@ require_relative '../helpers/token_helper'
 
 class UsersController < ApplicationController
   include TokenHelper
-  # rubocop: disable Style/GuardClause
+  rescue_from ActionController::ParameterMissing, with: :render_parameter_missing
+
+  def signin
+    User.find_by(email: user_params[:email]).then do |user|
+      unless user&.authenticate(params[:user][:password])
+        return render_error_response(401, 'Incorrect email/password.')
+      end
+
+      render_success_response(200, user.id)
+    end
+  end
+
   def signup
-    user = User.new(user_params)
-    if user.save
-      token = generate_token({ exp: (Time.now + 1800).to_i, user_id: user.id })
-      response.status = 201
+    User.new(user_params(true)).then do |user|
+      render_success_response(201, user.id) if user.save!
+    rescue ActiveRecord::RecordInvalid => e
+      render_error_response(403, e)
+    end
+  end
+
+  private
+
+  def render_error_response(status, message)
+    response.status = status
+    render json: {
+      errors: [
+        { title: message }
+      ]
+    }
+  end
+
+  def render_parameter_missing
+    render_error_response(400, 'Missing parameter(s).')
+  end
+
+  def render_success_response(status, user_id)
+    response.status = status
+    generate_token({ exp: (Time.now + 1800).to_i, user_id: user_id }).then do |token|
       render json: {
         data: {
           token: token
@@ -15,13 +47,12 @@ class UsersController < ApplicationController
       }
     end
   end
-  # rubocop: enable Style/GuardClause
 
-  private
-
-  def user_params
+  def user_params(name_required = false)
     params.require(:user).permit(:email, :name, :password).tap do |user_params|
-      user_params.require([:email, :name, :password])
+      (name_required ? [:email, :name, :password] : [:email, :password]).then do |p|
+        user_params.require(p)
+      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
