3344004adc0cbe822d5d25dcbc038151ba8f4a71
Yorkshireman <>
2023-04-06 00:30:34 +0100

    allow all origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 
-            /http:\/\/localhost:[0-9]\z/
-            /https:\/\/[a-z0-9-]+.herokuapp.com\z/
+    origins '*',
     resource  '*',
               headers: :any,
               methods: [:post, :options]
