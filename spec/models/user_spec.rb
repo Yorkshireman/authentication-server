605a90a4b5b591d83e21572dc8f7a81ae63ff815
Yorkshireman <>
2023-09-20 00:41:45 +0100

    fix test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,8 +37,8 @@ RSpec.describe User, type: :model do
 
   it 'throws error when attempting to be created, using `create!`, with the same name as a pre-existing User' do
     user
-    expect { User.create!(email: 'legit@email.com', name: 'Joe', password: 'password') }.to
-    raise_error(ActiveRecord::RecordInvalid)
+    expect { User.create!(email: 'legit@email.com', name: 'Joe', password: 'password') }
+      .to raise_error(ActiveRecord::RecordInvalid)
   end
 end
 # rubocop:enable Metrics/BlockLength
