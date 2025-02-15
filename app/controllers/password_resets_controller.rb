3c145e1636b4d6030a2376bd2fc56d934748770a
Yorkshireman <>
2025-02-15 23:25:24 +0000

    basic route outline

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,11 @@
 class PasswordResetsController < ApplicationController
-  def create; end
+  def create
+    # this route should take an email address as a parameter and check whether or not it exists
+    # if it does, it should send an email to that address with a link to the edit route
+    email = params.require(:email)
+    user = User.find_by(email: email)
+    puts user
+  end
 
   def edit; end
 
