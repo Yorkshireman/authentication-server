8b094314a39d28c005a48e7bae386cf827f581f9
Yorkshireman <>
2025-02-16 17:06:52 +0000

    write requests unit tests for controller

---
+++ spec/requests/reset_password_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+# rubocop:disable Metrics/BlockLength
+RSpec.describe 'POST /reset-password', type: :request do
+  before :all do
+    User.destroy_all
+    @user = User.create(email: 'test@test.com', name: 'test', password: 'password')
+  end
+
+  describe 'missing email param' do
+    before :all do
+      headers = {
+        'CONTENT_TYPE' => 'application/vnd.api+json'
+      }
+
+      post '/reset-password', headers: headers
+    end
+
+    it 'response is 400' do
+      expect(response).to have_http_status(400)
+    end
+  end
+
+  describe 'email not found' do
+    before :all do
+      headers = {
+        'CONTENT_TYPE' => 'application/vnd.api+json'
+      }
+
+      params = JSON.generate({ email: 'foo@bar.com' })
+      post '/reset-password', headers: headers, params: params
+    end
+
+    it 'response is 204 No Content' do
+      expect(response).to have_http_status(204)
+    end
+  end
+
+  describe 'email is found' do
+    before :all do
+      headers = {
+        'CONTENT_TYPE' => 'application/vnd.api+json'
+      }
+
+      params = JSON.generate({ email: 'test@test.com' })
+      post '/reset-password', headers: headers, params: params
+    end
+
+    it 'response is 204 No Content' do
+      expect(response).to have_http_status(204)
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength
