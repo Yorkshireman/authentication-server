58ab4eeb02be01d52eb050acc489406e1c52a501
Yorkshireman <>
2025-02-16 01:42:38 +0000

    generate token

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,10 +1,15 @@
+require_relative '../helpers/token_helper'
+
+# decoded_token = JWT.decode(token, ENV['JWT_SECRET_KEY'], true, { algorithm: 'HS256' })
 class PasswordResetsController < ApplicationController
+  include TokenHelper
   def create
-    # this route should take an email address as a parameter and check whether or not it exists
-    # if it does, it should send an email to that address with a link to the edit route
     email = params.require(:email)
     user = User.find_by(email: email)
-    puts user
+    return unless user
+
+    token = generate_token({ exp: (Time.now + 7200).to_i })
+    puts token
   end
 
   def edit; end
