d1ddaa7e3da216e44f09e6213e1730931a628d40
Yorkshireman <>
2025-03-01 16:51:44 +0000

    tests for expired token

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -18,7 +18,12 @@ RSpec.describe PasswordResetsController, type: :controller do
   end
 
   let(:expired_jwt) do
-    # Time.now - 1 ensures the token is expired
+    JWT.encode({ exp: (Time.now - 7300).to_i, issued_at: Time.now + 1, user_id: user.id }, ENV['JWT_SECRET_KEY'],
+               'HS256')
+  end
+
+  let(:used_jwt) do
+    # Time.now - 1 ensures the token is used
     JWT.encode({ exp: (Time.now + 7200).to_i, issued_at: Time.now - 1, user_id: user.id }, ENV['JWT_SECRET_KEY'],
                'HS256')
   end
@@ -54,9 +59,27 @@ RSpec.describe PasswordResetsController, type: :controller do
     end
 
     describe 'when token has already been used' do
-      it 'returns a bad request status and does not update the user\'s password' do
+      it 'returns a 422 status and does not update the user\'s password' do
+        patch :update, params: { token: used_jwt, password: 'new_password', password_confirmation: 'new_password' }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)['errors'][0]).to eq(
+          # rubocop:disable Layout/LineLength
+          'This password reset link has already been used. If you still need to reset your password, please request a new reset link.'
+          # rubocop:enable Layout/LineLength
+        )
+        expect(user.password).to eq('password')
+      end
+    end
+
+    describe 'when token has expired' do
+      it 'returns a 422 status and does not update the user\'s password' do
         patch :update, params: { token: expired_jwt, password: 'new_password', password_confirmation: 'new_password' }
         expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)['errors'][0]).to eq(
+          # rubocop:disable Layout/LineLength
+          'This password reset link has expired. If you still need to reset your password, please request a new reset link.'
+          # rubocop:enable Layout/LineLength
+        )
         expect(user.password).to eq('password')
       end
     end
