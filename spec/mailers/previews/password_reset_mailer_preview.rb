a3c2313736fed83e53f2b571c2ece8e1b4fe8cb7
Yorkshireman <>
2025-03-03 23:23:35 +0000

    delete code comment

--- spec/mailers/previews/password_reset_mailer_preview.rb
+++ spec/mailers/previews/password_reset_mailer_preview.rb
@@ -1,4 +1,3 @@
-# Preview all emails at http://localhost:3000/rails/mailers/password_reset
 class PasswordResetMailerPreview < ActionMailer::Preview
   def password_reset_email
     PasswordResetMailer.with(email: 'example@example.com', token: 'example_token').password_reset_email
