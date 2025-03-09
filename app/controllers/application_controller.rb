969e3e47273331764fedb3c4e10fb868685cb499
Andrew Stelmach <Yorkshireman@users.noreply.github.com>
2025-03-09 18:16:39 +0000

    Split routes and controllers into api and non-api (#63)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
-class ApplicationController < ActionController::API
-  before_action :set_headers
-
-  def set_headers
-    response.headers['Content-Type'] = 'application/vnd.api+json'
-  end
+class ApplicationController < ActionController::Base
 end
--- spec/requests/reset_password_spec.rb
+++ spec/requests/reset_password_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 # rubocop:disable Metrics/BlockLength
-RSpec.describe 'POST /reset-password', type: :request do
+RSpec.describe 'POST /api/reset-password', type: :request do
   before :all do
     User.destroy_all
     @user = User.create(email: 'test@test.com', name: 'test', password: 'password')
@@ -12,10 +12,8 @@ RSpec.describe 'POST /reset-password', type: :request do
         'CONTENT_TYPE' => 'application/vnd.api+json'
       }
 
-      post '/reset-password', headers: headers
-
       expect do
-        post '/reset-password', headers: headers
+        post '/api/reset-password', headers: headers
       end.not_to(change { ActionMailer::Base.deliveries.count })
 
       expect(response).to have_http_status(400)
@@ -30,7 +28,7 @@ RSpec.describe 'POST /reset-password', type: :request do
       params = JSON.generate({ email: 'foo@bar.com' })
 
       expect do
-        post '/reset-password', headers: headers, params: params
+        post '/api/reset-password', headers: headers, params: params
       end.not_to(change { ActionMailer::Base.deliveries.count })
 
       expect(response).to have_http_status(204)
@@ -44,10 +42,10 @@ RSpec.describe 'POST /reset-password', type: :request do
       }
 
       params = JSON.generate({ email: 'test@test.com' })
-      post '/reset-password', headers: headers, params: params
+      post '/api/reset-password', headers: headers, params: params
 
       expect do
-        post '/reset-password', headers: headers, params: params
+        post '/api/reset-password', headers: headers, params: params
       end.to(change { ActionMailer::Base.deliveries.count }.by(1))
 
       expect(response).to have_http_status(204)
